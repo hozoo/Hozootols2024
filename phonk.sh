d396d46e5ae97b80b4d2bb322609c02e728d8a23
hozoo <143112689+hozoo@users.noreply.github.com>
2023-10-17 09:53:03 +0700

    Selamat mencoba
    #!/bin/bash
    
    # Install Git
    pkg install git -y
    
    # Clone the repository
    git clone https://github.com/hozoo
    
    # Install Python
    pkg install python -y
    
    # Update the system
    apt update && apt upgrade -y
    
    # Install OpenSSL
    pkg install openssl -y
    
    # Install Python packages
    pip install requests colorama
    
    # Change directory to the cloned repository
    cd push-dc
    
    # Run the sh script
    sh phonk.sh

---
+++ phonk.sh
@@ -0,0 +1,106 @@
+#batas gan
+clear
+bi='\033[34;1m' #biru
+i='\033[32;1m' #ijo
+pur='\033[35;1m' #purple
+cy='\033[36;1m' #cyan
+me='\033[31;1m' #merah
+pu='\033[37;1m' #purple
+ku='\033[33;1m' #kuning
+echo $cy"██╗░░██╗░█████╗░███████╗░█████╗░░█████╗░    "
+echo $cy"██║░░██║██╔══██╗╚════██║██╔══██╗██╔══██╗"
+echo $cy"███████║██║░░██║░░███╔═╝██║░░██║██║░░██║"
+echo $cy"██╔══██║██║░░██║██╔══╝░░██║░░██║██║░░██║"
+echo $cy"██║░░██║╚█████╔╝███████╗╚█████╔╝╚█████"
+echo $cy"╚═╝░░╚═╝░╚════╝░╚══════╝░╚════╝░░╚════╝░"
+echo $cy"-----------------"$pur"-------------------"
+echo $cy" Author : Hozoo👑"
+echo $cy" Tiktok:hackershozoo"$pur"✓"
+echo $cy" 2023-10-15 update sc"$pur"✓"
+echo $cy"-----------------"$pur"-------------------" 
+sleep 2
+echo $bi "Pilih" $i "Yang" $cy "Mana" $pu "[?]"
+sleep 1
+echo [1]Clear
+echo [2]login termux
+echo [3]buka aplikasi Tiktok
+echo [4]join grup hackershozoo
+echo [5]membuat text ascii
+echo [6]membuka akun github anda
+echo [7]akun github admin
+echo [8] membuka vidio jj hackershozoo
+echo [9] subcribe hackershozoo
+read -p "[?] Choice : " pil :
+#Batas Gan
+if [ $pil = 1 ]
+then
+clear
+sleep 1
+figlet "cleared"
+sleep 1
+echo "Terimakasih Telah Mencoba Script Kami"
+echo "Jangan Lupa follow Channel hackershozoo"
+sleep 1
+echo $ku "Byee"
+fi
+#batas gan
+if [ $pil = 2 ]
+then
+clear
+login
+sleep 1
+figlet "logged"
+sleep 2
+echo "terimakasih telah mencoba script kami"
+sleep 1
+echo "jangan lupa follow channel hackershozoo"
+fi
+#batas gan
+if [ $pil = 3 ]
+then
+xdg-open https://www.tiktok.com/@hackershozoo?_t=8fsVbQZgx2w&_r=1
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 4 ]
+then
+xdg-open https://chat.whatsapp.com/KHj7b2WDMfn03aaY90XYqn
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 5 ]
+then
+xdg-open http://patorjk.com/software/taag/#p=display&f=Graffiti&t=Type%20Something%20
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 6 ]
+then
+xdg-open https://github.com
+sleep 2
+sh toolsTPA.sh
+fi
+#batas gan
+if [ $pil = 7 ]
+then
+xdg-open https://github.com/hozo
+sleep 2
+sh toolsTPA.sh
+fi
+#batas Gan
+if  [ $pil = 8 ]
+then
+xdg-open https://youtube.com/shorts/9k9szEtUPZw?si=yzzvRKtryzxJRhYq
+sleep2
+sh toolsTPA
+fi
+#batas Gan
+if  [ $pil  = 9 ]
+then
+xdg-open https://youtube.com/@hackerHozoo-vx2uo?si=wOVnRsMrIhaRrmFX
+clear
+fi
+       
